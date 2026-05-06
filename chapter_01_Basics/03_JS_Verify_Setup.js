d41cd3112e245a66a5acb64d1693d97590f616eb
mohammedazharuddin0215 <mohammed.azharuddin0215@gmail.com>
2026-05-06 08:24:34 +0530

    Initial commit

---
+++ chapter_01_Basics/03_JS_Verify_Setup.js
@@ -0,0 +1,3 @@
+console.log("Platform: " , process.platform);
+console.log("Architecture: " , process.arch);
+console.log("Node.js Version: " , process.version);
---
+++ chapter_01_Basics/04_HotCode.js
@@ -0,0 +1,9 @@
+for( let a = 1; a <= 10000; a++)
+    {
+        console.log(a);
+        print(a);
+    }
+function print(a)
+{
+    console.log("Hello");
+}
